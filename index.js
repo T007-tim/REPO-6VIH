5bd7df1f91cb249e0e882fdfb7b65c824303e4f6
timothy-mwau <timmulwa123@gmail.com>
2026-01-14 11:53:31 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+console.log("Building solutions for impact across Africa continent.")
+console.log("Hello+Variables")
+let firstName = "Timothy";
+let secondName = "Mulwa";
+let lastName = "Mwau";
+let age = "18";
+let profession = "Developer";
+
+console.log("Name:", firstName, secondName, lastName);
+console.log("Age:", age);
+console.log("Profession:", profession);
