31a50a74af5272a0ad5ef41127e9f707b3fb18f5
timothy-mwau <timmulwa123@gmail.com>
2026-01-17 17:22:19 +0300

    Add files via upload

---
+++ taskapplication.py
@@ -0,0 +1,12 @@
+#A  newly built task mangement appication
+tasks=[]  #list to store tasks
+def add_task(task):
+    task = {
+        "title":title,
+        "description":description,
+        "completed":False
+    }
+tasks.append(tasks)
+def  view_tasks():
+    return tasks
+print("Welcome to my Task Management Application")
